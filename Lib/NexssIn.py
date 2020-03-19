721395a7e304dbbd644185f2104af7e5d79f58bc
Marcin Polak <mapoart@gmail.com>
2020-03-19 22:56:49 +0100

    Revert back to the old version

--- Lib/NexssIn.py
+++ Lib/NexssIn.py
@@ -8,3 +8,39 @@ from NexssLog import nxsInfo, nxsOk, nxsWarn, nxsError
 
 NexssStdin = sys.stdin.read()
 NexssStdout = json.loads(NexssStdin)
+
+# We check if there is attribute 'nxsInAs' - this will get different default attrinute IN
+try:
+    inField_1 = NexssStdout['nxsInAs']
+except KeyError:
+    inField_1 = "nxsIn"
+
+# No parameters added so we take Output from previous module - intuitive workflow
+if not inField_1 in NexssStdout and "nxsOut" in NexssStdout:
+    inField_1 = "nxsOut"
+
+# We get value of the IN attribute
+try:
+    Field_Value_1 = NexssStdout[inField_1]
+except KeyError:
+    Field_Value_1 = None
+
+if Field_Value_1 is None:
+    try:
+        Field_Value_1 = NexssStdout['_'].copy()
+        del NexssStdout['_']    
+    except KeyError:        
+        Field_Value_1 = None
+
+# We got IN value inside Field_Value_1 so we delete _ var
+# if  '_' in NexssStdout:
+#     del NexssStdout['_']
+
+# Result field name to pass to next module
+try:
+    NexssStdout['resultField_1'] = NexssStdout['nxsOutAs']
+except KeyError:
+    NexssStdout['resultField_1'] = "nxsOut"
+
+if Field_Value_1:
+    NexssStdout['nxsIn'] = Field_Value_1
