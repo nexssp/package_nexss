17f55a619c7fcef7c588d41b96e9d0ea73c97dce
Marcin Polak <mapoart@gmail.com>
2020-09-02 07:26:11 +0200

    fixed typo

--- Test/Errors/src/errors1.js
+++ Test/Errors/src/errors1.js
@@ -1,11 +1,11 @@
 // Nexss PROGRAMMER 2.0.0 - NodeJS
 // Default template for JSON Data
 // STDIN
-const sleep = milliseconds => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-process.stdin.on("data", function(NexssStdin) {
+process.stdin.on("data", function (NexssStdin) {
   let NexssStdout;
   try {
     NexssStdout = JSON.parse(NexssStdin.toString());
@@ -18,7 +18,7 @@ process.stdin.on("data", function(NexssStdin) {
 
   const x = 1;
 
-  console.warn("Warning from file (Between erros No 1/2 and 2/2)");
+  console.warn("Warning from file (Between errors No 1/2 and 2/2)");
   const y = 1;
 
   console.error("Error 2/2 from file 1: Standard STDERR");
@@ -37,7 +37,7 @@ process.stdin.on("data", function(NexssStdin) {
   process.stdout.write(JSON.stringify(NexssStdout));
 });
 
-process.stdin.on("end", function() {
+process.stdin.on("end", function () {
   //On Windows below is not needed.
   process.exit(0);
 });
