410fcd3510924aac985fec48716c16820dc0b556
Marcin Polak <mapoart@gmail.com>
2020-10-11 13:58:14 +0200

    nxs functions now allow for mutliple arguments and different types.

--- Lib/NexssLog.js
+++ Lib/NexssLog.js
@@ -1,25 +1,25 @@
-const nxsInfo = (info) => {
-  console.error(`NEXSS/info:${info}`);
+const nxsInfo = (...info) => {
+  console.error(`NEXSS/info:`, ...info);
 };
 
-const nxsOk = (ok) => {
-  console.error(`NEXSS/ok:${ok}`);
+const nxsOk = (...ok) => {
+  console.error(`NEXSS/ok:`, ...ok);
 };
 
-const nxsError = (error) => {
-  console.error(`NEXSS/error:${error}`);
+const nxsError = (...error) => {
+  console.error(`NEXSS/error:`, ...error);
 };
 
-const nxsWarn = (warn) => {
-  console.error(`NEXSS/warn:${warn}`);
+const nxsWarn = (...warn) => {
+  console.error(`NEXSS/warn:`, ...warn);
 };
 
-const nxsTrace = (trace) => {
-  console.error(`NEXSS/trace:${trace}`);
+const nxsTrace = (...trace) => {
+  console.error(`NEXSS/trace:`, ...trace);
 };
 
-const nxsDebug = (debug) => {
-  console.error(`NEXSS/debug:${debug}`);
+const nxsDebug = (...debug) => {
+  console.error(`NEXSS/debug:`, ...debug);
 };
 
 module.exports = { nxsInfo, nxsOk, nxsError, nxsWarn, nxsTrace, nxsDebug };
